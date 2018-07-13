918a8d1848cd6c16f677da198dde87b39c81c735
Justin K Singh <justin.oliver.singh@gmail.com>
2018-07-13 11:10:29 -0700

    Update VideoFrame2.py
    **Second update
     CAN EXTRACT FRAMES AT 1 SECOND INTERVALS
    Push to array and database pics to array for comparison
    For every frame collected check against every pic in database and:
    First greyscale both pics to compare
    Resize so that they are the same dimensions
    Use SSIM structural similarity testing from skiimages to find a comparison percentage
    If percentage is below a tolerance, frame will be deleted from the EXPORT directory slow since it needs to run through every picture in the database for every frame. Could potentially just be my computer. Need to check

--- SignRecognition/VideoFrame2.py
+++ SignRecognition/VideoFrame2.py
@@ -7,18 +7,25 @@ import os
 import keyboard
 import glob
 import numpy as np
+from skimage.measure import compare_ssim as ssim
+import skimage.transform
+import matplotlib.pyplot as plt
+import scipy
 
 dir = "/home/jsingh/Codeblocks Projects/PROJECT/SignRecognition/TEST_VIDS/big_buck_bunny_720p_5mb.mp4"
+
+
 vidcap = cv2.VideoCapture(dir)
 success,image = vidcap.read()
 count = 0  # initialize counter
-while success and 1:
-  vidcap.set(cv2.cv.CV_CAP_PROP_POS_MSEC,(count*1000))    # Change multiple on count for number of frames taken  
+while success:
+  vidcap.set(cv2.cv.CV_CAP_PROP_POS_MSEC,(count*5000))    # Change multiple on count for number of frames taken  
   cv2.imwrite("/home/jsingh/Codeblocks Projects/PROJECT/SignRecognition/DATA/EXPORT/frame%d.jpg" % count, image)     # save frame as JPEG file
   success,image = vidcap.read()
-  print('Read a new frame: ', success)
+  print('Read a new frame: ', success, ' ', count)
   count += 1
   comp = False
+
 if success == False:
   print('DONE SPLICING VIDEO INTO FRAMES')
   Y_data = [] # Frames from video
@@ -41,5 +48,30 @@ else:
 
 if comp == True:
   print('NOW WILL COMPARE ARRAYS')
+  tolerance = 0.9
+  counted = 0
+  deleted =0
+  for i in range(0,len(Y_data)):
+     for j in range(0,len(X_data)):
+       Databased = cv2.cvtColor(X_data[j], cv2.COLOR_BGR2GRAY)
+       Exported = cv2.cvtColor(Y_data[i], cv2.COLOR_BGR2GRAY)
+       Databased = skimage.transform.resize(Databased, Exported.shape, mode='constant')
+       s = ssim(Exported, Databased)
+       WantToDelete = True
+       if s > tolerance:
+	 counted+=1
+         print("Files still in folder: ", counted)
+	 WantToDelete = False
+     if WantToDelete:    
+     	#delete picture from database
+	print("{}".format(export[i]))	
+	os.remove("{}".format(export[i]))	
+     	deleted+=1
+     	#print("Files deleted: ", deleted)
+  #print Databased.shape
+  #print Exported.shape
+  #s = ssim(original, original)
+  #print("MSE: ", m)
+  #print ("SSIM: ", s)
 else:
   print('I still suck')
